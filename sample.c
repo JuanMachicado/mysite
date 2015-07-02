c8ca121c3b0de6c7127897d8a18e9caed6e8d04a
Juan Machicado Sanjines <juan_machicado@jmachicados-dh01.jalasoft.local>
2015-07-02 16:58:16 -0400

    test

--- sample.c
+++ sample.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 int main() {
-   printf("hello %+-0.2lf world",2.3);
+   printf("hello %+-0.222222lf world",2.3);
    //just a comment
+   test 2
+	test 3
+	test 4
+	a	
+
    return 0;
 }
